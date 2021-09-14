a723cf60745c57434cd9bb47e178dc35184633be
Renaro Santos <renaropsantos@gmail.com>
2021-09-14 21:23:58 +0200

    First implementation of Product Repository

--- app/src/main/java/com/renarosantos/ecommerceapp/ProductListViewModel.kt
+++ app/src/main/java/com/renarosantos/ecommerceapp/ProductListViewModel.kt
@@ -3,6 +3,8 @@ package com.renarosantos.ecommerceapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class ProductListViewModel : ViewModel() {
 
@@ -10,17 +12,14 @@ class ProductListViewModel : ViewModel() {
     val viewState: LiveData<ProductListViewState>
         get() = _viewState
 
+    private val repository = ProductRepository()
 
     fun loadProductList() {
-        _viewState.postValue(ProductListViewState.Loading)
-        // Data call to fetch products
-        _viewState.postValue(ProductListViewState.Content((1..3).map {
-            ProductCardViewState(
-                "Playstation $it",
-                "This is a nice console! Check it out",
-                "200 US$",
-                "https://firebasestorage.googleapis.com/v0/b/androidecommercesample.appspot.com/o/playstation_1.png?alt=media&token=1414f40e-23cf-4f44-b922-e12bfcfca9f3"
-            )
-        }))
+        viewModelScope.launch {
+            _viewState.postValue(ProductListViewState.Loading)
+            // Data call to fetch products
+            val productList = repository.getProductList()
+            _viewState.postValue(ProductListViewState.Content(productList))
+        }
     }
 }
---
+++ app/src/main/java/com/renarosantos/ecommerceapp/ProductRepository.kt
@@ -0,0 +1,22 @@
+package com.renarosantos.ecommerceapp
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+class ProductRepository {
+
+    suspend fun getProductList(): List<ProductCardViewState> {
+        return withContext(Dispatchers.IO) {
+            delay(2000)
+            (1..3).map {
+                ProductCardViewState(
+                    "Playstation $it",
+                    "This is a nice console! Check it out",
+                    "200 US$",
+                    "https://firebasestorage.googleapis.com/v0/b/androidecommercesample.appspot.com/o/playstation_1.png?alt=media&token=1414f40e-23cf-4f44-b922-e12bfcfca9f3"
+                )
+            }
+        }
+    }
+}
